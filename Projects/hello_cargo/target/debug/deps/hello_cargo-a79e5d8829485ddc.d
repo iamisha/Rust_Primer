a3e5cb7fd2609154f1887c937ae7f215d2d2260f
iamisha <isha.201710@ncit.edu.np>
2023-04-26 20:45:05 +0545

    hello_cargo project added 🥈

---
+++ Projects/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/iamisha/Documents/rust_/Projects/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/iamisha/Documents/rust_/Projects/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ Projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/iamisha/Documents/rust_/Projects/hello_cargo/target/debug/hello_cargo: /home/iamisha/Documents/rust_/Projects/hello_cargo/src/main.rs
