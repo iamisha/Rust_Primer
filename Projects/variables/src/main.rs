6e9c5c9accf931ba090e14ac107bad8b14b9e4d6
iamisha <isha.201710@ncit.edu.np>
2023-04-28 23:19:25 +0545

    constants and shadowing  readme file created :arrows_clockwise:

--- Projects/variables/src/main.rs
+++ Projects/variables/src/main.rs
@@ -2,9 +2,30 @@ fn main() {
    // let x = 5; gives error
 
     let mut x = 5;
-    
+
     println!("The value of x is : {x}");
 
     x = 6;
     println!("The value of x is : {x}");
+
+    // constants
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("{THREE_HOURS_IN_SECONDS} seconds in 3 hours.");
+
+    // shadowing
+    let x = 7;
+    let x  = 7 + 1; // declaring new variable x with the same name as previous variable
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is : {x}");
+    }
+    println!("The value of x is: {x}");
+
+    // another purpose
+
+    // let mut spaces = " "; gives error
+    let spaces = "      ";
+    let spaces = spaces.len();
+    println!("Total spaces available are: {spaces}");
 }
